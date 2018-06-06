e62cde5c0408eeb038f7d398f8b5e48456730181
chioreandan <chiorean.dan12@gmail.com>
2018-06-06 12:37:52 +0300

    Add about controller and view and add test for about section

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -11,4 +11,9 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "shout get about" do
+    get static_pages_about_url
+    assert_response :success
+  end
+
 end
