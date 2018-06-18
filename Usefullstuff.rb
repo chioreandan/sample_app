b306fc61205b5f13799e6e5f822a40cb32d72390
chioreandan <chiorean.dan12@gmail.com>
2018-06-18 15:43:02 +0300

    make a basic user model(including password

---
+++ Usefullstuff.rb
@@ -0,0 +1,30 @@
+rails console --sandbox #to start rails console that rollbacks on exit
+
+
+User.all #returns all users
+User.find(1) #return first user
+User.find_by(email: "email@email.com") #return user where email== email
+User.find_by_name("Dan") #return user where name == "Dan"
+
+#--UPDATE USER
+user.email="update@email.com"
+user.save
+user.reload #to see what happens if user.save
+
+user.update_attributes(name:"Update", email:"Update@update.com") #update multiple attributes is to use update_attributes
+user.update_attribute(:name, "Update")  #update 1 attribute
+
+
+##validate
+
+validates :name, presence: true
+
+validates :name,  presence: true, length: { maximum: 50 }
+validates :email, presence: true, length: { maximum: 255 }
+
+
+
+$ rails generate migration add_index_to_users_email
+
+
+rails generate migration add_password_digest_to_users password_digest:string
