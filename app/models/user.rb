35f76d50f7da0bd2bbf27090509178896f3b13c7
chioreandan <chiorean.dan12@gmail.com>
2018-06-27 16:28:35 +0300

    Add user feed

--- app/models/user.rb
+++ app/models/user.rb
@@ -38,7 +38,10 @@ class User < ApplicationRecord
   end
 
   def feed
-    Micropost.where("user_id = ?",id)
+    following_ids = "SELECT followed_id FROM relationships
+                     WHERE  follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids})
+                     OR user_id = :user_id", user_id: id)
   end
 
   def self.digest(string)
@@ -103,5 +106,6 @@ class User < ApplicationRecord
   def following?(other_user)
     following.include?(other_user)
   end
+
 
 end
